1e2f1049aca16c15f07c7f12dc649025cdbaa428
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2026-07-08 17:58:20 +0530

    Url Extractor

--- Array/Url_extractor.js
+++ Array/Url_extractor.js
@@ -1,20 +1,53 @@
 let url = "http://google.com/example?name=mojahid&age=18&gender=male";
-// console.log(url.indexOf("name"))
-// console.log(url.slice(26))
-function processUrl(url){
-    return function (key){
-        if(!url.includes(key)){
-            return null;
-        }else{
-            let index = url.indexOf(key);
-            console.log("Index---",index)
-            let andSign = url.indexOf('&')
-            console.log("And Sign",andSign);
-            let sign  = url.substring(index,andSign)
-            console.log("Sign---",sign)
-            return sign;
+console.log(url?.split("?")[1].split('&'))
+const array = url?.split("?")[1].split('&');
+const result = {}
+array.forEach((item,index)=>{
+    const [key,value] = item.split("=")
+    result[key] = value
+})
+function checkValue(result,key){
+    for(let i in result){
+        console.log(i,result[i])
+        if(i === key){
+            return result[i];
         }
     }
 }
+console.log(checkValue(result,"name"))
+// console.log(array.length)
+// for(let i = 0;i<array.length;i++){
+//     console.log(value[i].flat())
+// }
+// function processUrl(url) {
+//     const obj = {};
+//     url?.split("?")?.split("&")?.split("=")?.map((item)=>{
+//         console.log(item);
+//     });
 
-console.log(processUrl(url)("name"))
+//     return function (key) {
+//         let search = key + "=";
+
+//         let start = url.indexOf(search);
+
+//         if (start === -1) {
+//             return null;
+//         }
+
+//         start += search.length;
+
+//         let end = url.indexOf("&", start);
+
+//         if (end === -1) {
+//             end = url.length;
+//         }
+
+//         return url.substring(start, end);
+//     };
+// }
+
+// const getParam = processUrl(url);
+
+// console.log(getParam("name"));   // mojahid
+// console.log(getParam("age"));    // 18
+// console.log(getParam("gender")); // male
